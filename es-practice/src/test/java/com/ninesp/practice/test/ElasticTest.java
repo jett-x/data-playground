26e9b517a67d8f7b59811d656a1ca98a8461a5cc
ninesp <ninesp@foxmail.com>
2023-05-23 22:46:53 +0800

    geo search

--- es-practice/src/test/java/com/ninesp/practice/test/ElasticTest.java
+++ es-practice/src/test/java/com/ninesp/practice/test/ElasticTest.java
@@ -10,19 +10,12 @@ package com.ninesp.practice.test;
 
 import cn.hutool.core.bean.BeanUtil;
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.ninesp.practice.dal.entity.RidingOrderDO;
 import com.ninesp.practice.dal.mapper.RidingOrderMapper;
 import com.ninesp.practice.domain.Location;
 import com.ninesp.practice.domain.RidingOrder;
 import com.ninesp.practice.util.GeoHashConverter;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.Map;
-import javax.annotation.Resource;
 import org.elasticsearch.action.bulk.BulkRequest;
 import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.index.IndexRequest;
@@ -32,14 +25,21 @@ import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.indices.CreateIndexRequest;
 import org.elasticsearch.client.indices.CreateIndexResponse;
+import org.elasticsearch.common.unit.DistanceUnit;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.fetch.subphase.FetchSourceContext;
 import org.elasticsearch.search.sort.FieldSortBuilder;
 import org.elasticsearch.search.sort.SortOrder;
 import org.junit.jupiter.api.Test;
 import org.springframework.util.CollectionUtils;
+
+import javax.annotation.Resource;
+import java.io.IOException;
+import java.util.List;
 
 /**
  * @author chenjun
@@ -118,14 +118,46 @@ public class ElasticTest extends AbstractAppTest{
 
     @Test
     public void testRawQuery() throws IOException {
-        SearchRequest searchRequest = new SearchRequest("movies");
+        SearchRequest searchRequest = new SearchRequest("riding_order");
         SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
-        searchSourceBuilder.query(QueryBuilders.rangeQuery("rating").gt(8.0));
-        searchSourceBuilder.sort(new FieldSortBuilder("release_date").order(SortOrder.DESC));
+        searchSourceBuilder.query(QueryBuilders.rangeQuery("userId").gt(12000));
+        searchSourceBuilder.sort(new FieldSortBuilder("startTime").order(SortOrder.DESC));
         searchRequest.source(searchSourceBuilder);
 
         SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
         for (SearchHit hit : response.getHits().getHits()) {
+            System.out.println(hit.getSourceAsString());
+        }
+    }
+
+    @Test
+    public void testGeoSearch() throws IOException {
+        this.searchWithinDistance("riding_order", 39.96963, 116.34933, 10, DistanceUnit.KILOMETERS);
+    }
+
+    private void searchWithinDistance(String index, double lat, double lon, double distance, DistanceUnit unit)
+        throws IOException {
+        SearchRequest searchRequest = new SearchRequest(index);
+
+        // 构建查询
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+        boolQueryBuilder.filter(QueryBuilders.geoDistanceQuery("startLoc").point(lat, lon).distance(distance, unit));
+        sourceBuilder.query(boolQueryBuilder);
+
+        // 设置返回结果包含哪些字段
+        String[] includes = new String[] {"userId", "orderId", "endLoc"};
+        String[] excludes = new String[] {"startLoc", "bikeId"};
+        FetchSourceContext fetchSourceContext = new FetchSourceContext(true, includes, excludes);
+        sourceBuilder.fetchSource(fetchSourceContext);
+
+        searchRequest.source(sourceBuilder);
+
+        // 执行搜索
+        SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
+
+        // 处理搜索结果
+        for (SearchHit hit : searchResponse.getHits().getHits()) {
             System.out.println(hit.getSourceAsString());
         }
     }
